38099e2aa773a2ece49ce31c0e87067c60994d7c
williamsmata <williams.rm99@gmail.com>
2024-04-05 11:42:47 -0400

    Create theme context, integration with react navigation and react native paper

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,15 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {PaperProvider} from 'react-native-paper';
 import StackNavigator from './presentation/navigator/StackNavigator';
+import {ThemeContextProvider} from './presentation/context/ThemeContext';
 
 export default function PokedexApp() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <ThemeContextProvider>
+      <StackNavigator />
+    </ThemeContextProvider>
   );
 }
