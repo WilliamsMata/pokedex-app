68f471d128350da740ada575feaa34af2662441f
williamsmata <williams.rm99@gmail.com>
2024-04-05 11:29:54 -0400

    Add react-native-paper and button dependency in HomeScreen.tsx

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {PaperProvider} from 'react-native-paper';
 import StackNavigator from './presentation/navigator/StackNavigator';
 
 export default function PokedexApp() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {Button} from 'react-native-paper';
 
 export default function HomeScreen() {
   return (
     <View>
       <Text>HomeScreen</Text>
+
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
   );
 }
