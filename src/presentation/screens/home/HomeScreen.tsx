372d397a20d7aa5599ce840b509449a60ed4f5b8
williamsmata <williams.rm99@gmail.com>
2024-04-05 16:04:20 -0400

    Tanstack query installed and get pokemons

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,27 @@
-import {View} from 'react-native';
 import React from 'react';
-import {Button, Text} from 'react-native-paper';
+import {View} from 'react-native';
+import {ActivityIndicator, Button, Text} from 'react-native-paper';
+import {useQuery} from '@tanstack/react-query';
+import {getPokemons} from '../../../actions/pokemons';
 
 export default function HomeScreen() {
+  const {isLoading, data} = useQuery({
+    queryKey: ['pokemons'],
+    queryFn: () => getPokemons(),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+
   return (
     <View>
       <Text variant="displaySmall">HomeScreen</Text>
 
-      <Button mode="contained" onPress={() => console.log('Pressed')}>
-        Press me
-      </Button>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+      )}
     </View>
   );
 }
